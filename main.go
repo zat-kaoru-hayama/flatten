1a23c45181b03d08f0c983c9aca4cf86b524392e
HAYAMA_Kaoru <kaoru.hayama@alfatech.jp>
2020-04-17 15:36:52 +0900

    See msm folder to compare

--- main.go
+++ main.go
@@ -22,14 +22,18 @@ func main() {
 						return errors.New("two few arguments")
 					}
 					approvedRoot := args[0]
-					flatDir := args[1]
+					installerRoot := args[1]
+					flatDir := filepath.Join(installerRoot, "flat")
+					msmDir := filepath.Join(installerRoot, "msm")
 
 					matched := make(map[string]int)
+					flatReferred := make(map[string]bool)
 
 					filepath.Walk(approvedRoot, func(path string, f os.FileInfo, err error) error {
 						if f.IsDir() {
 							return nil
 						}
+						// test Flat
 						basename := filepath.Base(path)
 						stubpath := filepath.Join(flatDir, basename)
 						_, err = os.Stat(stubpath)
@@ -55,8 +59,26 @@ func main() {
 						} else {
 							matched[name]--
 						}
+						flatReferred[name] = false
 						return nil
 					})
+					filepath.Walk(msmDir, func(path string, f os.FileInfo, err error) error {
+						if f.IsDir() {
+							return nil
+						}
+						name := strings.ToUpper(f.Name())
+						if _, ok := flatReferred[name]; !ok {
+							fmt.Printf("[ERR] Not found in Flat: %s\n", path)
+						} else {
+							flatReferred[name] = true
+						}
+						return nil
+					})
+					for name, refer := range flatReferred {
+						if !refer {
+							fmt.Printf("[ERR] Not referred from MSM: %s\n", name)
+						}
+					}
 					return nil
 				},
 			},
